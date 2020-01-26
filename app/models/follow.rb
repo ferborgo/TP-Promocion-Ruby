519d84022a1de9494d90f7f69422943af68fe20d
fernando <fer.borgognoni@gmail.com>
2020-01-26 20:55:22 -0300

    Notifications for new follower working

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -11,4 +11,21 @@ class Follow < ActiveRecord::Base
     self.update_attribute(:blocked, true)
   end
 
+  after_create :create_notification
+
+  def create_notification
+    Notification.create do |notification|
+      notification.notify_type = 'follower'
+
+      # Who creates the notification
+      notification.actor = self.follower
+
+      # Who receives the notification.
+      notification.user = self.followable
+
+      notification.target = self
+
+    end
+  end
+
 end
