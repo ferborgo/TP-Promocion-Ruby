64168c45a7c16042a15ead7275baf6fe8420ad79
fernando <fer.borgognoni@gmail.com>
2020-01-27 17:22:01 -0300

    User can edit Profile. Pretty name for Book Status

--- app/models/book_user.rb
+++ app/models/book_user.rb
@@ -2,7 +2,18 @@ class BookUser < ApplicationRecord
   self.table_name = "books_users"
   enum status: [ :reading, :finished, :abandoned, :to_read ]
 
+  STATUS_NAMES = {
+    reading: 'reading :D',
+    finished: 'finished!',
+    abandoned: 'abandoned :(',
+    to_read: 'to read...'
+  }
+
   belongs_to :book
   belongs_to :user
   paginates_per 5
+
+  def self.pretty_name(param)
+    BookUser::STATUS_NAMES[param.parameterize.underscore.to_sym]
+  end
 end
