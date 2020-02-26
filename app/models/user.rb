58d32ccb8fb071eb839fa872369f5f32e819cad9
fernando <fer.borgognoni@gmail.com>
2020-02-26 19:19:15 -0300

    Username added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-  validates :email, uniqueness: true
-  
+  validates :email, :username, uniqueness: true
+
   has_and_belongs_to_many :books
   has_many :opinions
   has_many :recommendations
