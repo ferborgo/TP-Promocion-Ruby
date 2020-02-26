6a352a74e2d74f6f57fd4cda7ac5048bf1700298
fernando <fer.borgognoni@gmail.com>
2020-02-26 16:24:56 -0300

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users = User.create([
+  {email: 'admin@admin.com', is_admin: true, first_name: 'Admin', last_name: 'Admin', username: 'Admin'},
+  {email: 'example@gmail.com', is_admin: false, first_name: 'Juan', last_name: 'Pérez', username: 'juanperez'}
+])
+
 genres = Genre.create([
     {name: 'Drama'},
     {name: 'Assay'},
-    {name: 'Fantastic'}
+    {name: 'Fantastic'},
+    {name: 'Novel'},
+    {name: 'Literatura Contemporánea'}
 ])
 
 authors = Author.create([
   {first_name: 'Jorge Luis', last_name: 'Borges'},
-  {first_name: 'Ernest', last_name: 'Sábato'},
+  {first_name: 'Ernesto', last_name: 'Sábato'},
   {first_name: 'Gabriel', last_name: 'García Márquez'},
-  {first_name: 'Ray', last_name: 'Bradbury'}
+  {first_name: 'Ray', last_name: 'Bradbury'},
+  {first_name: 'Julio', last_name: 'Cortázar'}
 ])
 
 books = Book.create([
@@ -25,5 +33,47 @@ books = Book.create([
     author: Author.find_by(last_name: 'Borges'),
     genre: Genre.find_by(name: 'Fantastic'),
     pages: 145
+  },
+  {
+    title: 'Fahrenheit 451',
+    author: Author.find_by(last_name: 'Bradbury'),
+    genre: Genre.find_by(name: 'Fantastic'),
+    pages: 350
+  },
+  {
+    title: 'El túnel',
+    author: Author.find_by(last_name: 'Sábato'),
+    genre: Genre.find_by(name: 'Literatura Contemporánea'),
+    pages: 240
+  },
+  {
+    title: 'Sobre héroes y tumbas',
+    author: Author.find_by(last_name: 'Sábato'),
+    genre: Genre.find_by(name: 'Literatura Contemporánea'),
+    pages: 540
+  },
+  {
+    title: 'Los funerales de Mamá Grande',
+    author: Author.find_by(last_name: 'García Márquez'),
+    genre: Genre.find_by(name: 'Fantastic'),
+    pages: 240
+  },
+  {
+    title: 'Rayuela',
+    author: Author.find_by(last_name: 'Cortázar'),
+    genre: Genre.find_by(name: 'Novel'),
+    pages: 690
+  },
+  {
+    title: '62 Modelo para Armar',
+    author: Author.find_by(last_name: 'Cortázar'),
+    genre: Genre.find_by(name: 'Novel'),
+    pages: 490
+  },
+  {
+    title: 'Libro de Manuel',
+    author: Author.find_by(last_name: 'Cortázar'),
+    genre: Genre.find_by(name: 'Novel'),
+    pages: 500
   }
 ])
